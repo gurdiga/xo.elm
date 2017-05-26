58a76cf1bfd5ded68749c19799d1b234f264751d
Vlad GURDIGA <gurdiga@gmail.com>
2017-05-26 10:02:05 +0300

    Form validation!

--- src/Main.elm
+++ src/Main.elm
@@ -1,46 +1,119 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Regex
+
+
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+
+
+-- MODEL
 
 
 type alias Model =
-    Int
+    { name : String
+    , password : String
+    , passwordAgain : String
+    , age : String
+    , showValidationResults : Bool
+    }
 
 
-main : Program Never Model Msg
-main =
-    Html.beginnerProgram
-        { model = 0
-        , view = view
-        , update = update
-        }
+model : Model
+model =
+    { name = ""
+    , password = ""
+    , passwordAgain = ""
+    , age = ""
+    , showValidationResults = False
+    }
+
+
+
+-- UPDATE
 
 
 type Msg
-    = Increment
-    | Decrement
-    | Reset
+    = Name String
+    | Password String
+    | PasswordAgain String
+    | Age String
+    | Check
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            model + 2
+        Name name ->
+            { model | showValidationResults = False, name = name }
 
-        Decrement ->
-            model - 2
+        Password password ->
+            { model | showValidationResults = False, password = password }
 
-        Reset ->
-            0
+        PasswordAgain password ->
+            { model | showValidationResults = False, passwordAgain = password }
+
+        Age age ->
+            { model | showValidationResults = False, age = age }
+
+        Check ->
+            { model | showValidationResults = True }
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , text (toString model)
-        , button [ onClick Increment ] [ text "+" ]
-        , button [ onClick Reset ] [ text "reset" ]
+        [ input [ type_ "text", placeholder "Name", onInput Name ] []
+        , input [ type_ "password", placeholder "Password", onInput Password ] []
+        , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+        , input [ type_ "text", placeholder "How old are you?", onInput Age ] []
+        , button [ onClick Check ] [ text "Check!" ]
+        , if model.showValidationResults then
+            viewValidation model
+          else
+            div [] []
         ]
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+    let
+        hasAge =
+            String.length model.age > 0
+
+        hasPassword =
+            String.length model.password > 0 || String.length model.passwordAgain > 0
+
+        ( color, message ) =
+            if hasAge && not (isNumber model.age) then
+                ( "tomato", "Age should be a whole positive number" )
+            else if hasPassword && not (isComplexEnough model.password) then
+                ( "tomato", "Password is too simple" )
+            else if hasPassword && String.length model.password < 8 then
+                ( "brown", "Password too short!" )
+            else if hasPassword && model.password /= model.passwordAgain then
+                ( "red", "Passwords do not match!" )
+            else
+                ( "green", "OK" )
+    in
+        div [ style [ ( "color", color ) ] ] [ text message ]
+
+
+isComplexEnough : String -> Bool
+isComplexEnough string =
+    Regex.contains (Regex.regex "[A-Z]") string
+        && Regex.contains (Regex.regex "[a-z]") string
+        && Regex.contains (Regex.regex "[0-9]") string
+
+
+isNumber : String -> Bool
+isNumber string =
+    Regex.contains (Regex.regex "[0-9]") string

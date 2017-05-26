7221051991a35758a0bef8cc379f2cbf72e6ee16
Vlad GURDIGA <gurdiga@gmail.com>
2017-05-26 09:11:18 +0300

    The reset feature 8-)

--- src/Main.elm
+++ src/Main.elm
@@ -4,27 +4,43 @@ import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 
+type alias Model =
+    Int
+
+
+main : Program Never Model Msg
 main =
-    Html.beginnerProgram { model = 0, view = view, update = update }
+    Html.beginnerProgram
+        { model = 0
+        , view = view
+        , update = update
+        }
 
 
 type Msg
     = Increment
     | Decrement
+    | Reset
 
 
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            model + 2
 
         Decrement ->
-            model - 1
+            model - 2
+
+        Reset ->
+            0
 
 
+view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model) ]
+        , text (toString model)
         , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Reset ] [ text "reset" ]
         ]

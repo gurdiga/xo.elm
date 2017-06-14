5d17591ac96c1da1abef6af826b9175d4c34011d
Vlad GURDIGA <gurdiga@gmail.com>
2017-06-14 10:01:09 +0300

    More type schetching

--- .vimrc
+++ .vimrc
@@ -1,3 +1,6 @@
 set viminfo+=n.viminfo
 
 let g:elm_format_autosave = 1
+
+" Make the selected text a comment, then wrap lines.
+vmap gC gcgpgq
--- src/Main.elm
+++ src/Main.elm
@@ -34,13 +34,9 @@ type alias ID =
     String
 
 
-
--- type alias Grounds =
---     String
-
-
 type Grounds
     = CreditorPetition CreditorPetitionValue
+    | MortgageCreditorPetition MortgageCreditorPetitionValue
     | CourtRequest CourtRequestValue
     | Takeover TakeoverValue
 
@@ -48,6 +44,23 @@ type Grounds
 type alias CreditorPetitionValue =
     { creditor : Person
     , petition : Petition
+    }
+
+
+type alias MortgageCreditorPetitionValue =
+    { -- • originalul contractului de ipotecă învestit cu formulă executorie⁺
+      mortgageContract : MortgageContract MortgageContractValue
+    , -- • copia contractului de credit bancar sau de împrumut⁺
+      creditAgreement : CreditAgreement CreditAgreementValue
+    , -- • extrasele de evidenţă financiară⁺
+      financialRecords : FinancialRecords FinancialRecordsValue
+    , -- • copiile notificării şi preavizului expediate pe adresa debitorului
+      notice : ScannedImage
+    , intimation : ScannedImage
+    , -- • declaraţia pe propria răspundere a creditorului că nu există un
+      -- litigiu judiciar în legătură cu contractul dat de ipotecă
+      -- (ăf′ĭ-dā′vĭt)
+      affidavit : MortgageCreditorNoLitigationAffidavit
     }
 
 
@@ -64,8 +77,42 @@ type alias CourtRequestValue =
     }
 
 
-type Cause
-    = Cause
+type
+    -- (2) Instanţa de judecată prezintă din oficiu titlul executoriu spre
+    -- executare în pricinile ce ţin:
+    Cause
+    = -- a) de confiscarea bunurilor;
+      AssetConfiscation
+    | -- b) de urmărirea sumelor ce urmează a fi făcute venit la stat;
+      MoneyCollectionForGovernment
+    | -- c) urmărirea sumelor încasate din contul statului, din contul
+      -- întreprinderilor de stat şi al celor municipale, al societăţilor
+      -- comerciale cu capital majoritar de stat;
+      MoneyCollectionFromGovernment
+    | -- d) de urmărirea pensiei de întreţinere;
+      AlimonyCollection
+    | -- e) de încasarea sumelor pentru repararea prejudiciilor cauzate prin
+      -- vătămarea integrităţii corporale, prin o altă vătămare a sănătăţii sau
+      -- prin deces, dacă repararea s-a efectuat sub formă de prestaţii băneşti
+      -- periodice;
+      DamagePaymentCollection
+    | -- e1) de repararea prejudiciului cauzat prin încălcarea dreptului la
+      -- judecarea în termen rezonabil a cauzei sau a dreptului la executarea în
+      -- termen rezonabil a hotărîrii judecătoreşti;
+      RightInfringementRepair
+    | -- e2) de repararea prejudiciului cauzat prin acţiunile ilicite ale
+      -- organelor de urmărire penală, ale procuraturii şi ale instanţelor
+      -- judecătoreşti;
+      IllicitActionRepair
+    | -- f) de restabilirea la locul de muncă şi de încasarea salariului mediu
+      -- pentru întreaga perioadă de absenţă forţată de la muncă;
+      JobRestoration
+    | -- g) încasarea indemnizaţiilor pentru incapacitate temporară de muncă şi
+      -- altor prestaţii de asigurări sociale prevăzute de lege;
+      SocialInsuranceCompensation
+    | -- h) de încasarea cheltuielilor pentru acordarea asistenţei juridice
+      -- garantate de stat.
+      LegalAssistancePaymentCollection
 
 
 type alias TakeoverValue =

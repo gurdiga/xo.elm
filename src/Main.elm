ab735627fa7938e92e4f8784868c872174fc4642
Vlad GURDIGA <gurdiga@gmail.com>
2017-06-20 10:01:50 +0300

    More type sketching + some UI sketching for Grounds

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (value, selected)
+import Html.Attributes exposing (value, selected, style)
 import Html.Events exposing (..)
+import Date exposing (Date)
 import Select
 
 
@@ -29,30 +30,30 @@ type alias Model =
 type alias Process =
     { id : ID
     , grounds : Grounds
-    , courtOrder : CourtOrderValue
+    , order : OrderValue
     }
 
 
 type alias
     -- https://workflowy.com/#/07d4c20e89a4
-    CourtOrderValue
+    OrderValue
     =
     { -- • denumirea instanţei de judecată care l-a eliberat
       court : CourtValue
     , -- • pricina în a cărei bază a fost eliberat, categoria de creanță —
       -- https://workflowy.com/#/6e5a4774a675
-      debtCategory : DebtCategory
+      cause : OrderCause
     , -- • data pronunţării hotărîrii
-      orderDate : Date
+      orderDate : Maybe Date
     , -- • dispozitivul (textual)
       text : String
     , -- • data rămînerii definitive a hotărîrii — https://workflowy.com/#/a3557fcba688
-      finalDate : Date
+      finalDate : Maybe Date
     , -- • date despre debitor(i) și creditor — https://workflowy.com/#/62b772bb8008
       creditor : Person
     , debtors : List Person
     , -- • data eliberării titlului executoriu
-      releaseDate : Date
+      releaseDate : Maybe Date
     , -- • dacă sunt, documentele executorii eliberate de instanţa de judecată
       -- prin care s-a dispus aplicarea măsurilor de asigurare a acţiunii sau de
       -- asigurare a probelor, sau de anulare a lor¹
@@ -70,8 +71,79 @@ type alias
     }
 
 
+newOrder : OrderValue
+newOrder =
+    { court = "The Court"
+    , cause = OrderCause DebtCategory1 ChildSupportDebt
+    , orderDate = Nothing
+    , text = "You shall do this and that."
+    , finalDate = Nothing
+    , creditor = newPerson
+    , debtors = [ newPerson ]
+    , releaseDate = Nothing
+    , warrants = [ newWarrant ]
+    , addressForKeepingSeizedAssets = ""
+    , notes = ""
+    }
+
+
+type alias CourtValue =
+    String
+
+
+type OrderCause
+    = OrderCause DebtCategory Debt
+
+
+type
+    -- Articolul 145. Categoriile de creanţe
+    -- (1) Se stabilesc 3 categorii de creanţe.
+    -- (2) La categoria întîi de creanţe sînt atribuite creanţele privind:
+    Debt
+    = -- a) plata pensiei de întreţinere;
+      ChildSupportDebt
+    | -- b) plăţile ce decurg din raporturile de muncă;
+      EmploymentRelatedDebt
+    | -- c) reparaţia prejudiciilor cauzate prin schilodire sau prin o altă
+      -- vătămare a sănătăţii, precum şi în legătură cu pierderea
+      -- întreţinătorului;
+      HealthDamageDebt
+    | -- d) reparaţia prejudiciilor cauzate persoanelor fizice prin infracţiune
+      -- sau contravenţie;
+      PersonalDamageDebt
+    | -- e) contribuţiile obligatorii de asigurări sociale de stat.
+      SocialSecurityDebt
+      -- (3) La categoria a doua de creanţe sînt atribuite creanţele privind:
+    | -- a) impozitele, taxele şi alte plăţi aferente bugetului public naţional;
+      TaxDebt
+    | -- b) taxele de stat, amenzile şi penalităţile aferente bugetului public
+      -- naţional;
+      PenaltiesDebt
+    | -- c) reparaţia prejudiciilor cauzate prin infracţiune sau contravenţie
+      -- persoanelor juridice, precum şi asociaţiilor şi societăţilor care nu au
+      -- personalitate juridică, dar care dispun de organe de conducere proprii;
+      CorporationDamageDebt
+    | -- d) plăţile pentru drepturile de autor şi drepturile conexe;
+      CopyrightPenaltiesDebt
+    | -- e) cheltuielile de asistenţă juridică.
+      JudicialAssistanceDebt
+    | -- (4) La categoria a treia de creanţe sînt atribuite celelalte creanţe.
+      OtherDebt
+
+
+type DebtCategory
+    = DebtCategory1
+    | DebtCategory2
+    | DebtCategory3
+
+
 type Warrant
     = Warrant -- Fields + Maybe ScannedImageValue
+
+
+newWarrant : Warrant
+newWarrant =
+    Warrant
 
 
 type alias ID =
@@ -79,8 +151,8 @@ type alias ID =
 
 
 type Grounds
-    = CourtDecision CourtDecisionValue -- public policy, public interest
-    | CreditorPetition CreditorPetitionValue
+    = CreditorPetition CreditorPetitionValue
+    | CourtDecision CourtDecisionValue -- public policy, public interest
     | MortgageCreditorPetition MortgageCreditorPetitionValue
     | Takeover TakeoverValue
 
@@ -190,7 +262,7 @@ newPetition =
 
 
 type alias CourtDecisionValue =
-    { cause : Cause
+    { cause : CourtDecisionCause
     }
 
 
@@ -203,7 +275,7 @@ newCourtDecisionValue =
 type
     -- (2) Instanţa de judecată prezintă din oficiu titlul executoriu spre
     -- executare în pricinile ce ţin:
-    Cause
+    CourtDecisionCause
     = -- a) de confiscarea bunurilor;
       AssetConfiscation
     | -- b) de urmărirea sumelor ce urmează a fi făcute venit la stat;
@@ -260,6 +332,7 @@ newProcess : Process
 newProcess =
     { id = "001"
     , grounds = CreditorPetition newCreditorPetitionValue
+    , order = newOrder
     }
 
 
@@ -292,24 +365,60 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "Procedură nouă" ]
-        , label []
-            [ text "Temeiul:"
-            , Select.fromValuesWithLabels groundsWithLabels ChangeGrounds (currentGrounds model)
+    let
+        currentGrounds =
+            case model.openedProcess of
+                Nothing ->
+                    CourtDecision newCourtDecisionValue
+
+                Just process ->
+                    process.grounds
+    in
+        div []
+            [ h1 [] [ text "Procedură nouă" ]
+            , label []
+                [ text "Temeiul:"
+                , Select.fromValuesWithLabels groundsWithLabels ChangeGrounds currentGrounds
+                , groundsFields currentGrounds
+                ]
+            , pre [ style [ ( "white-space", "normal" ) ] ] [ text (toString model) ]
             ]
-        , pre [] [ text (toString model) ]
-        ]
 
 
-currentGrounds : Model -> Grounds
-currentGrounds model =
-    case model.openedProcess of
-        Nothing ->
-            CourtDecision newCourtDecisionValue
+groundsFields : Grounds -> Html Msg
+groundsFields grounds =
+    case grounds of
+        CourtDecision decision ->
+            courtDecisionFields decision
 
-        Just openedProcess ->
-            openedProcess.grounds
+        CreditorPetition creditorPetition ->
+            creditorPetitionFields creditorPetition
+
+        MortgageCreditorPetition mortgageCreditorPetition ->
+            mortgageCreditorPetitionFields mortgageCreditorPetition
+
+        Takeover takeover ->
+            takeoverFields takeover
+
+
+courtDecisionFields : CourtDecisionValue -> Html Msg
+courtDecisionFields decision =
+    div [] [ text <| "CourtDecision" ++ (toString decision) ]
+
+
+creditorPetitionFields : CreditorPetitionValue -> Html Msg
+creditorPetitionFields creditorPetition =
+    div [] [ text <| "CreditorPetition" ++ (toString creditorPetition) ]
+
+
+mortgageCreditorPetitionFields : MortgageCreditorPetitionValue -> Html Msg
+mortgageCreditorPetitionFields mortgageCreditorPetition =
+    div [] [ text <| "MortgageCreditorPetition" ++ (toString mortgageCreditorPetition) ]
+
+
+takeoverFields : TakeoverValue -> Html Msg
+takeoverFields takeover =
+    div [] [ text <| "Takeover" ++ (toString takeover) ]
 
 
 groundsWithLabels : List ( Grounds, String )

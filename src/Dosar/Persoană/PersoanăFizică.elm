79060bd7140d10df5600e699aadb2eb598e20446
Vlad GURDIGA <gurdiga@gmail.com>
2017-06-24 16:06:22 +0300

    PersoanăFizică: more fields

--- src/Dosar/Persoană/PersoanăFizică.elm
+++ src/Dosar/Persoană/PersoanăFizică.elm
@@ -1,12 +1,18 @@
 module Dosar.Persoană.PersoanăFizică exposing (PersoanăFizică, newValue, view)
 
-import Html exposing (Html, fieldset, legend, label, input, text)
+import Html exposing (Html, fieldset, legend, ul, li, label, input, textarea, text)
+import Html.Attributes exposing (value)
 import Html.Events exposing (onInput)
+import Date exposing (Date)
 
 
 type alias PersoanăFizică =
     { nume : String
     , prenume : String
+    , dataNașterii : Maybe Date
+    , cnp : String
+    , adresa : String -- Address?
+    , note : String
     }
 
 
@@ -14,6 +20,10 @@ newValue : PersoanăFizică
 newValue =
     { nume = ""
     , prenume = ""
+    , dataNașterii = Nothing
+    , cnp = ""
+    , adresa = ""
+    , note = ""
     }
 
 
@@ -21,12 +31,37 @@ view : PersoanăFizică -> (PersoanăFizică -> msg) -> Html msg
 view persoană callback =
     fieldset []
         [ legend [] [ text "PersoanăFizică" ]
-        , label []
-            [ text "Nume:"
-            , input [ onInput (\v -> callback { persoană | nume = v }) ] []
+        , ul []
+            [ li [] [ textField "Nume:" persoană.nume (\v -> callback { persoană | nume = v }) ]
+            , li [] [ textField "Prenume:" persoană.prenume (\v -> callback { persoană | prenume = v }) ]
+
+            --, li [] [ textField "Data nașterii:" persoană.dataNașterii (\v -> callback { persoană | dataNașterii = v }) ]
+            , li [] [ textField "CNP:" persoană.cnp (\v -> callback { persoană | cnp = v }) ]
+            , li [] [ largeTextField "Adresa:" persoană.adresa (\v -> callback { persoană | adresa = v }) ]
+            , li [] [ largeTextField "Note:" persoană.note (\v -> callback { persoană | note = v }) ]
             ]
-        , label []
-            [ text "Prenume:"
-            , input [ onInput (\v -> callback { persoană | prenume = v }) ] []
+        ]
+
+
+textField : String -> String -> (String -> msg) -> Html msg
+textField labelText defaultValue callback =
+    label []
+        [ text labelText
+        , input
+            [ value defaultValue
+            , onInput callback
             ]
+            []
+        ]
+
+
+largeTextField : String -> String -> (String -> msg) -> Html msg
+largeTextField labelText defaultValue callback =
+    label []
+        [ text labelText
+        , textarea
+            [ value defaultValue
+            , onInput callback
+            ]
+            []
         ]

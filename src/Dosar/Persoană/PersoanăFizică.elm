44086cd02639e71ba08cef16dca03d09ff67a1e0
Vlad GURDIGA <gurdiga@gmail.com>
2017-06-25 10:30:02 +0300

    Sketching out a dateField (not working properly)

--- src/Dosar/Persoană/PersoanăFizică.elm
+++ src/Dosar/Persoană/PersoanăFizică.elm
@@ -9,7 +9,7 @@ import Date exposing (Date)
 type alias PersoanăFizică =
     { nume : String
     , prenume : String
-    , dataNașterii : Maybe Date
+    , dataNașterii : Result String Date
     , cnp : String
     , adresa : String -- Address?
     , note : String
@@ -20,7 +20,7 @@ newValue : PersoanăFizică
 newValue =
     { nume = ""
     , prenume = ""
-    , dataNașterii = Nothing
+    , dataNașterii = Err ""
     , cnp = ""
     , adresa = ""
     , note = ""
@@ -34,8 +34,7 @@ view persoană callback =
         , ul []
             [ li [] [ textField "Nume:" persoană.nume (\v -> callback { persoană | nume = v }) ]
             , li [] [ textField "Prenume:" persoană.prenume (\v -> callback { persoană | prenume = v }) ]
-
-            --, li [] [ textField "Data nașterii:" persoană.dataNașterii (\v -> callback { persoană | dataNașterii = v }) ]
+            , li [] [ dateField "Data nașterii:" persoană.dataNașterii (\v -> callback { persoană | dataNașterii = v }) ]
             , li [] [ textField "CNP:" persoană.cnp (\v -> callback { persoană | cnp = v }) ]
             , li [] [ largeTextField "Adresa:" persoană.adresa (\v -> callback { persoană | adresa = v }) ]
             , li [] [ largeTextField "Note:" persoană.note (\v -> callback { persoană | note = v }) ]
@@ -65,3 +64,70 @@ largeTextField labelText defaultValue callback =
             ]
             []
         ]
+
+
+dateField : String -> Result String Date -> (Result String Date -> msg) -> Html msg
+dateField labelText defaultValue callback =
+    let
+        ( inputText, validationMessage ) =
+            case defaultValue of
+                Ok date ->
+                    ( formatDate date, "OK" )
+
+                Err errorMessage ->
+                    ( "", errorMessage )
+
+        formatDate date =
+            (toString (Date.day date))
+                ++ "."
+                ++ (toString (monthNumber date))
+                ++ "."
+                ++ (toString (Date.year date))
+
+        monthNumber date =
+            case Date.month date of
+                Date.Jan ->
+                    1
+
+                Date.Feb ->
+                    2
+
+                Date.Mar ->
+                    3
+
+                Date.Apr ->
+                    4
+
+                Date.May ->
+                    5
+
+                Date.Jun ->
+                    6
+
+                Date.Jul ->
+                    7
+
+                Date.Aug ->
+                    8
+
+                Date.Sep ->
+                    9
+
+                Date.Oct ->
+                    10
+
+                Date.Nov ->
+                    11
+
+                Date.Dec ->
+                    12
+    in
+        label []
+            [ text labelText
+            , input
+                [ value inputText
+                , onInput (\v -> callback (Date.fromString v))
+                ]
+                []
+            , text validationMessage
+            ]

1fd2204b80ffa8bcb514eb4399c70946e3890638
Vlad GURDIGA <gurdiga@gmail.com>
2017-08-12 18:42:20 +0300

    PartileConvinAsupraConditiilorExecutarii

--- src/Dosar/Actiune/IncheiereIntentare/RezultatIncercareConciliere.elm
+++ src/Dosar/Actiune/IncheiereIntentare/RezultatIncercareConciliere.elm
@@ -1,28 +1,82 @@
 module Dosar.Actiune.IncheiereIntentare.RezultatIncercareConciliere exposing (RezultatIncercareConciliere, newValue, view)
 
-import Html exposing (Html, div, text)
-import Dosar.Actiune.IncheiereIntentare.RezultatIncercareConciliere.PartileAjungLaIntelegere as PartileAjungLaIntelegere exposing (PartileAjungLaIntelegere)
-
-
--- TODO: Review Articolul 62.: it looks like there can be nuances worthy of
--- encoding as type options.
+import Html exposing (Html, fieldset, legend, div, label, text)
+import Dosar.Actiune.IncheiereIntentare.RezultatIncercareConciliere.PartileConvinAsupraConditiilorExecutarii as PartileConvinAsupraConditiilorExecutarii exposing (PartileConvinAsupraConditiilorExecutarii)
+import Widgets.Select as Select
 
 
 type RezultatIncercareConciliere
-    = PartileAjungLaIntelegere PartileAjungLaIntelegere
+    = PartileConvinAsupraConditiilorExecutarii PartileConvinAsupraConditiilorExecutarii
+    | PartileConvinAsupraUneiTranzactii
+    | NeprezentareaUneiParti
     | PartileNuAjungLaIntelegere
 
 
 newValue : RezultatIncercareConciliere
 newValue =
-    PartileAjungLaIntelegere PartileAjungLaIntelegere.newValue
+    PartileConvinAsupraConditiilorExecutarii PartileConvinAsupraConditiilorExecutarii.newValue
 
 
-view : RezultatIncercareConciliere -> (RezultatIncercareConciliere -> msg) -> Html msg
+view : RezultatIncercareConciliere -> (RezultatIncercareConciliere -> Cmd msg -> Sub msg -> msg) -> Html msg
 view rezultatIncercareConciliere callback =
+    fieldset []
+        [ legend [] [ dropdown rezultatIncercareConciliere (\v -> callback v Cmd.none Sub.none) ]
+        , fields rezultatIncercareConciliere callback
+        ]
+
+
+dropdown : RezultatIncercareConciliere -> (RezultatIncercareConciliere -> msg) -> Html msg
+dropdown rezultatIncercareConciliere callback =
+    label []
+        [ text "RezultatIncercareConciliere:"
+        , Select.fromValuesWithLabels valuesWithLabels (defaultValue rezultatIncercareConciliere) callback
+        ]
+
+
+fields : RezultatIncercareConciliere -> (RezultatIncercareConciliere -> Cmd msg -> Sub msg -> msg) -> Html msg
+fields rezultatIncercareConciliere callback =
     case rezultatIncercareConciliere of
-        PartileAjungLaIntelegere rezultatIncercareConciliere ->
-            PartileAjungLaIntelegere.view rezultatIncercareConciliere (\v -> callback (PartileAjungLaIntelegere v))
+        PartileConvinAsupraConditiilorExecutarii v ->
+            PartileConvinAsupraConditiilorExecutarii.view v (callback << PartileConvinAsupraConditiilorExecutarii)
+
+        PartileConvinAsupraUneiTranzactii ->
+            div [] [ text <| toString rezultatIncercareConciliere ]
+
+        NeprezentareaUneiParti ->
+            div [] [ text <| toString rezultatIncercareConciliere ]
 
         PartileNuAjungLaIntelegere ->
             div [] [ text <| toString rezultatIncercareConciliere ]
+
+
+valuesWithLabels : List ( RezultatIncercareConciliere, String )
+valuesWithLabels =
+    [ ( PartileConvinAsupraConditiilorExecutarii PartileConvinAsupraConditiilorExecutarii.newValue
+      , "părțile convin asupra condițiilor de executare"
+      )
+    , ( PartileConvinAsupraUneiTranzactii
+      , "părțile convin asupra unei tranzacții"
+      )
+    , ( NeprezentareaUneiParti
+      , "părţile sau una dintre părţi nu se prezintă"
+      )
+    , ( PartileNuAjungLaIntelegere
+      , "părțile nu ajung la înțelegere"
+      )
+    ]
+
+
+defaultValue : RezultatIncercareConciliere -> RezultatIncercareConciliere
+defaultValue rezultatIncercareConciliere =
+    case rezultatIncercareConciliere of
+        PartileConvinAsupraConditiilorExecutarii _ ->
+            PartileConvinAsupraConditiilorExecutarii PartileConvinAsupraConditiilorExecutarii.newValue
+
+        PartileConvinAsupraUneiTranzactii ->
+            PartileConvinAsupraUneiTranzactii
+
+        NeprezentareaUneiParti ->
+            NeprezentareaUneiParti
+
+        PartileNuAjungLaIntelegere ->
+            PartileNuAjungLaIntelegere

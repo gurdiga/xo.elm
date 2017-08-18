95df128eec2668223c4aefe3597c991459e90637
Vlad GURDIGA <gurdiga@gmail.com>
2017-08-18 09:53:25 +0300

    Broken

--- src/Dosar/DocumentExecutoriu.elm
+++ src/Dosar/DocumentExecutoriu.elm
@@ -2,6 +2,9 @@ module Dosar.DocumentExecutoriu exposing (DocumentExecutoriu, newValue, view)
 
 import Html exposing (Html, fieldset, legend, br, text)
 import MyDate exposing (MyDate)
+import Utils.List as ListUtils
+import Widgets.Fields exposing (largeTextField)
+import Dosar.Persoana as Persoana exposing (Persoana)
 import Dosar.DocumentExecutoriu.Pricina as Pricina exposing (Pricina)
 import Dosar.DocumentExecutoriu.InstantaDeJudecata as InstantaDeJudecata exposing (InstantaDeJudecata)
 
@@ -14,6 +17,9 @@ type alias Data =
     { instantaEmitatoare : InstantaDeJudecata
     , pricina : Pricina
     , dataPronuntareHotarire : MyDate
+    , dispozitivul : String
+    , dataRamineriiDefinitive : MyDate
+    , debitori : List Persoana
     }
 
 
@@ -23,6 +29,9 @@ newValue =
         { instantaEmitatoare = InstantaDeJudecata.newValue
         , pricina = Pricina.newValue
         , dataPronuntareHotarire = MyDate.newValue
+        , dispozitivul = ""
+        , dataRamineriiDefinitive = MyDate.newValue
+        , debitori = [ Persoana.newValue ]
         }
 
 
@@ -42,4 +51,26 @@ view documentExecutoriu callback =
             , Pricina.view data.pricina (\v -> c { data | pricina = v })
             , br [] []
             , MyDate.view "Data pronunțării hotărîrii:" data.dataPronuntareHotarire (\v -> c { data | dataPronuntareHotarire = v })
+            , br [] []
+            , largeTextField "Dispozitivul:" data.dispozitivul (\v -> c { data | dispozitivul = v })
+            , br [] []
+            , MyDate.view "Data rămînerii definitive:" data.dataRamineriiDefinitive (\v -> c { data | dataRamineriiDefinitive = v })
+            , debitoriView data.debitori (\v -> c { data | debitori = v })
             ]
+
+
+debitoriView : List Persoana -> (List Persoana -> msg) -> Html msg
+debitoriView debitori callback =
+    fieldset []
+        ((legend [] [ text "Debitori" ])
+            :: (List.indexedMap
+                    (\i debitor ->
+                        fieldset []
+                            [ legend [] [ text (toString i) ]
+                            , text (toString debitor)
+                            , Persoana.view debitor (\v -> ListUtils.replace debitor i v)
+                            ]
+                    )
+                    debitori
+               )
+        )

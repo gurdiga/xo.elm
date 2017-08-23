b2281080c72b70aceabde749ddaa38d1efdc5141
Vlad GURDIGA <gurdiga@gmail.com>
2017-08-23 10:00:48 +0300

    Trying out elm-mdl (everything’s broken!)

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,12 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, pre, text)
+import Html exposing (Html, h1, div, pre, text)
 import Html.Attributes exposing (value, selected, style)
 import Dosar exposing (Dosar)
+import Material
+import Material.Scheme as Scheme
+import Material.Layout as Layout
+import Material.Color as Color
 
 
 main : Program Never Model Msg
@@ -19,6 +23,8 @@ type alias Model =
     { dosare : List Dosar
     , dosarDeschis : Maybe Dosar
     , subscription : Sub Msg
+    , mdl : Material.Model
+    , selectedTab : Int
     }
 
 
@@ -27,11 +33,14 @@ initialModel =
     { dosare = []
     , dosarDeschis = Just Dosar.empty
     , subscription = Sub.none
+    , mdl = Material.model
+    , selectedTab = 0
     }
 
 
 type Msg
     = Update Model (Cmd Msg) (Sub Msg)
+    | Mdl (Material.Msg Msg)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -39,6 +48,9 @@ update msg model =
     case msg of
         Update model cmd sub ->
             ( { model | subscription = sub }, cmd )
+
+        Mdl msg ->
+            Material.update msg model
 
 
 subscriptions : Model -> Sub Msg
@@ -48,7 +60,24 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div []
+    Scheme.topWithScheme Color.Teal Color.LightGreen <|
+        Layout.render Mdl
+            model.mdl
+            [ Layout.fixedHeader
+            , Layout.selectedTab model.selectedTab
+            , Layout.onSelectTab (\v -> Update { model | selectedTab = v } Cmd.none Sub.none)
+            ]
+            { header = [ h1 [ style [ ( "padding", "2rem" ) ] ] [ text "Counter" ] ]
+            , drawer = []
+            , tabs = ( [ text "Milk", text "Oranges" ], [] )
+            , main = [ viewBody model ]
+            }
+
+
+viewBody : Model -> Html Msg
+viewBody model =
+    div
+        []
         [ dosarView model
         , pre [ style [ ( "white-space", "normal" ) ] ] [ text (toString model) ]
         ]

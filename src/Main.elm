0368604cae2a36eadf81d6a9a61c2112ad7dca5e
Vlad GURDIGA <gurdiga@gmail.com>
2017-08-25 09:04:16 +0300

    Revert "Trying out elm-mdl (everything’s broken!)"
    This reverts commits:
    
    23b6807 elm-mdl: compiles!
    b228108 Trying out elm-mdl (everything’s broken!)
    
    It seems to introduce additional complexity, and I want to focus on
    domain modeling for now.
    
    Maybe later.

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,8 @@
 module Main exposing (..)
 
-import Html exposing (Html, h1, div, pre, text)
+import Html exposing (Html, div, pre, text)
 import Html.Attributes exposing (value, selected, style)
 import Dosar exposing (Dosar)
-import Material
-import Material.Scheme as Scheme
-import Material.Layout as Layout
-import Material.Color as Color
 
 
 main : Program Never Model Msg
@@ -23,8 +19,6 @@ type alias Model =
     { dosare : List Dosar
     , dosarDeschis : Maybe Dosar
     , subscription : Sub Msg
-    , mdl : Material.Model
-    , selectedTab : Int
     }
 
 
@@ -33,14 +27,11 @@ initialModel =
     { dosare = []
     , dosarDeschis = Just Dosar.empty
     , subscription = Sub.none
-    , mdl = Material.model
-    , selectedTab = 0
     }
 
 
 type Msg
     = Update Model (Cmd Msg) (Sub Msg)
-    | Mdl (Material.Msg Msg)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -48,9 +39,6 @@ update msg model =
     case msg of
         Update model cmd sub ->
             ( { model | subscription = sub }, cmd )
-
-        Mdl msg ->
-            Material.update Mdl msg model
 
 
 subscriptions : Model -> Sub Msg
@@ -60,24 +48,7 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    Scheme.topWithScheme Color.Teal Color.LightGreen <|
-        Layout.render Mdl
-            model.mdl
-            [ Layout.fixedHeader
-            , Layout.selectedTab model.selectedTab
-            , Layout.onSelectTab (\v -> Update { model | selectedTab = v } Cmd.none Sub.none)
-            ]
-            { header = [ h1 [ style [ ( "padding", "2rem" ) ] ] [ text "Counter" ] ]
-            , drawer = []
-            , tabs = ( [ text "Milk", text "Oranges" ], [] )
-            , main = [ viewBody model ]
-            }
-
-
-viewBody : Model -> Html Msg
-viewBody model =
-    div
-        []
+    div []
         [ dosarView model
         , pre [ style [ ( "white-space", "normal" ) ] ] [ text (toString model) ]
         ]

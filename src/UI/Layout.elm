fa04b97f7badaec826b4ebb98510979d3a28ce72
Vlad GURDIGA <gurdiga@gmail.com>
2017-10-21 16:02:59 +0300

    More Layout

--- src/UI/Layout.elm
+++ src/UI/Layout.elm
@@ -1,7 +1,8 @@
 module UI.Layout exposing (view)
 
-import Html exposing (Html, div, header, text)
-import Html.Attributes exposing (style)
+import Html exposing (Html, node, div, header, text)
+import Html.Attributes
+import Css exposing (..)
 
 
 type alias Input msg =
@@ -11,20 +12,69 @@ type alias Input msg =
 
 view : Input msg -> Html msg
 view { content } =
-    div [] ([ topBar ] ++ content)
+    div []
+        [ topBar
+        , container content
+        ]
 
 
 topBar : Html msg
 topBar =
     header
-        [ style
-            [ ( "position", "fixd" )
-            , ( "z-index", "1" )
-            , ( "box-shadow", "0 2px 5px rgba(0, 0, 0, 0.26)" )
-            , ( "padding", "1em" )
-            , ( "font-family", "'Alegreya Sans SC', sans" )
-            , ( "color", "white" )
-            , ( "background", "#ad1457" )
+        [ styles
+            [ position fixed
+            , top (px 0)
+            , width (pct 100)
+            , zIndex (int 1)
+            , boxShadow4 (px 0) (px headerShadowVerticalSpread) (px 5) (rgba 0 0 0 0.26)
+            , padding2 (px headerVerticalPadding) (em 1)
+            , --- TODO: add a `font` helper to accept a value of a union type which has the available fonts as options
+              fontFamilies [ "Alegreya SC" ]
+            , fontSize (px 22)
+            , lineHeight (px headerLineHeight)
+            , color (hex "fff")
+            , backgroundColor (hex "ad1457")
             ]
         ]
-        [ text "Top bar" ]
+        [ Html.text "Executori.Org" ]
+
+
+headerHeight : Float
+headerHeight =
+    headerLineHeight + headerVerticalPadding + headerShadowVerticalSpread
+
+
+headerMarginBottom : Float
+headerMarginBottom =
+    15
+
+
+headerLineHeight : Float
+headerLineHeight =
+    22
+
+
+headerVerticalPadding : Float
+headerVerticalPadding =
+    15
+
+
+headerShadowVerticalSpread : Float
+headerShadowVerticalSpread =
+    2
+
+
+styles : List Css.Style -> Html.Attribute msg
+styles =
+    Css.asPairs >> Html.Attributes.style
+
+
+container : List (Html msg) -> Html msg
+container content =
+    node "main"
+        [ styles
+            [ marginTop (px (headerHeight + headerMarginBottom))
+            , padding2 (px 0) (px 20)
+            ]
+        ]
+        content

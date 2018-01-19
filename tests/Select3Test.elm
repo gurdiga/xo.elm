d777f187b9d2a77670b2c10c428f42d8f6459857
Vlad GURDIGA <gurdiga@gmail.com>
2018-01-19 09:40:25 +0200

    Select3: test keyboard interactions

--- tests/Select3Test.elm
+++ tests/Select3Test.elm
@@ -16,7 +16,7 @@ markupStructure =
     describe "markup structure"
         [ test "wrapping container attributes" <|
             \_ ->
-                renderWithModel model
+                renderWithModel initialModel
                     |> Query.has
                         [ Selector.attribute <| attribute "role" "combobox"
                         , Selector.attribute <| attribute "aria-expanded" "false"
@@ -24,12 +24,12 @@ markupStructure =
                         ]
         , test "label" <|
             \_ ->
-                renderWithModel model
+                renderWithModel initialModel
                     |> Query.find [ Selector.tag "label" ]
                     |> Query.has [ Selector.text "A dropdown:" ]
         , test "input" <|
             \_ ->
-                renderWithModel model
+                renderWithModel initialModel
                     |> Query.find [ Selector.tag "input" ]
                     |> Query.has
                         [ Selector.attribute <| attribute "aria-autocomplete" "list"
@@ -44,21 +44,22 @@ clickingTheField =
     describe "when clicking the field"
         [ test "emits a Toggle message" <|
             \_ ->
-                renderWithModel model
+                renderWithModel initialModel
                     |> Query.find [ Selector.tag "input" ]
                     |> Event.simulate Event.click
                     |> Event.expect Select3.Toggle
         , describe "Toggle message"
             (let
                 updatedModel =
-                    Select3.update Select3.Toggle model
+                    Select3.update Select3.Toggle initialModel
 
                 rendered =
                     renderWithModel updatedModel
              in
              [ test "sets model’s isOpened to True (because it’s initially False)" <|
                 \_ ->
-                    Expect.equal (Select3.isOpened updatedModel) True
+                    Select3.isOpened updatedModel
+                        |> Expect.equal True
              , test "sets aria-expanded to true" <|
                 \_ ->
                     rendered
@@ -84,19 +85,19 @@ keyboardSupport =
         [ describe "down arrow"
             [ test "emits a KeyDown message with the appropriate key code" <|
                 \_ ->
-                    renderWithModel model
+                    renderWithModel initialModel
                         |> Query.find [ Selector.tag "input" ]
                         |> Event.simulate (keyDownEvent 40)
                         |> Event.expect (Select3.KeyDown 40)
             , test "opens the dropdown, initially" <|
                 \_ ->
-                    Select3.update (Select3.KeyDown 40) model
+                    Select3.update (Select3.KeyDown 40) initialModel
                         |> Select3.isOpened
                         |> Expect.equal True
             , describe "when it’s opened, and there was initially no hovered option"
                 (let
                     modelOpened =
-                        Select3.update Select3.Open model
+                        Select3.update Select3.Open initialModel
                  in
                  [ test "sets the first option as the hovered one" <|
                     \_ ->
@@ -119,7 +120,7 @@ keyboardSupport =
                                 ]
                  , test "the hover state is cleared when closed" <|
                     \_ ->
-                        model
+                        initialModel
                             |> Select3.update Select3.Open
                             |> Select3.update (Select3.KeyDown 40)
                             |> Select3.update Select3.Close
@@ -128,6 +129,94 @@ keyboardSupport =
                  ]
                 )
             ]
+        , describe "arrow up"
+            (let
+                modelOpened =
+                    Select3.update Select3.Open initialModel
+             in
+             [ test "sets the first option as the hovered one" <|
+                \_ ->
+                    modelOpened
+                        |> -- hover the first option
+                           Select3.update (Select3.KeyDown 40)
+                        |> -- hover the second option
+                           Select3.update (Select3.KeyDown 40)
+                        |> -- back to the first option
+                           Select3.update (Select3.KeyDown 38)
+                        |> Select3.hoveredValue
+                        |> Expect.equal (Just 1)
+             ]
+            )
+        , describe "pressing Enter"
+            (let
+                modelSelected =
+                    Select3.update Select3.Open initialModel
+                        |> -- hover the first option
+                           Select3.update (Select3.KeyDown 40)
+                        |> -- hover the second option
+                           Select3.update (Select3.KeyDown 40)
+                        |> -- Enter to select the second option
+                           Select3.update (Select3.KeyDown 13)
+             in
+             [ test "marks the hovered option as selected" <|
+                \_ ->
+                    Select3.selectedValue modelSelected |> Expect.equal 2
+             , test "marks the select ar closed" <|
+                \_ ->
+                    Select3.isOpened modelSelected |> Expect.equal False
+             , test "renders the listbox hidden" <|
+                \_ ->
+                    renderWithModel modelSelected
+                        |> Query.find [ Selector.attribute <| attribute "role" "listbox" ]
+                        |> Query.has [ Selector.style [ ( "display", "none" ) ] ]
+             ]
+            )
+        , describe "pressing Esc"
+            (let
+                modelClosed =
+                    Select3.update Select3.Open initialModel
+                        |> Select3.update (Select3.KeyDown 27)
+             in
+             [ test "marks the select as closed" <|
+                \_ ->
+                    modelClosed
+                        |> Select3.isOpened
+                        |> Expect.equal False
+             , test "renders the listbox hidden" <|
+                \_ ->
+                    renderWithModel modelClosed
+                        |> Query.find [ Selector.attribute <| attribute "role" "listbox" ]
+                        |> Query.has [ Selector.style [ ( "display", "none" ) ] ]
+             ]
+            )
+        , describe "pressing a letter"
+            (let
+                modelOpened =
+                    Select3.update Select3.Open initialModel
+             in
+             [ test "selects the option that begins wit that letter" <|
+                \_ ->
+                    modelOpened
+                        |> -- press “t”
+                           Select3.update (Select3.KeyDown 116)
+                        |> Select3.selectedValue
+                        |> Expect.equal 2
+             , test "selects the option that begins wit that letter (case insensitively)" <|
+                \_ ->
+                    modelOpened
+                        |> -- press “T”
+                           Select3.update (Select3.KeyDown 84)
+                        |> Select3.selectedValue
+                        |> Expect.equal 2
+             , test "disregards any other letter" <|
+                \_ ->
+                    modelOpened
+                        |> -- press “A”
+                           Select3.update (Select3.KeyDown 65)
+                        |> Select3.selectedValue
+                        |> Expect.equal (Select3.selectedValue initialModel)
+             ]
+            )
         ]
 
 
@@ -138,6 +227,6 @@ renderWithModel model =
         |> Query.fromHtml
 
 
-model : Select3.Model number
-model =
+initialModel : Select3.Model number
+initialModel =
     Select3.initialModel 1 [ ( 1, "one" ), ( 2, "two" ) ]

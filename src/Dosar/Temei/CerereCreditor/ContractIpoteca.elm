0b3890e0daf201c9b66fa673803fb0668a05e2aa
Vlad GURDIGA <gurdiga@gmail.com>
2018-01-24 09:59:18 +0200

    ContractIpoteca gone TEA

--- src/Dosar/Temei/CerereCreditor/ContractIpoteca.elm
+++ src/Dosar/Temei/CerereCreditor/ContractIpoteca.elm
@@ -9,6 +9,9 @@ import Widgets.MoneyField as MoneyField
 type Msg
     = SetObiect LargeTextField.Msg
     | SetValoareBunDePiata MoneyField.Msg
+    | SetValoareBunDeInlocuire MoneyField.Msg
+    | SetValoareDeBazaCreanta MoneyField.Msg
+    | SetNote LargeTextField.Msg
 
 
 update : Msg -> Model -> Model
@@ -19,6 +22,15 @@ update msg model =
 
         SetValoareBunDePiata moneyFieldMsg ->
             { model | valoareBunDePiata = MoneyField.update moneyFieldMsg model.valoareBunDePiata }
+
+        SetValoareBunDeInlocuire moneyFieldMsg ->
+            { model | valoareBunDeInlocuire = MoneyField.update moneyFieldMsg model.valoareBunDeInlocuire }
+
+        SetValoareDeBazaCreanta moneyFieldMsg ->
+            { model | valoareDeBazaCreanta = MoneyField.update moneyFieldMsg model.valoareDeBazaCreanta }
+
+        SetNote largeTextFieldMsg ->
+            { model | note = LargeTextField.update largeTextFieldMsg model.note }
 
 
 type alias Model =
@@ -44,23 +56,9 @@ view : Model -> Html Msg
 view contractIpoteca =
     fieldset []
         [ legend [] [ text "ContractIpoteca" ]
-        , ul []
-            [ li [] [ LargeTextField.view "Obiectul ipotecii:" contractIpoteca.obiect |> map SetObiect ]
-            , li [] [ MoneyField.view "Valoarea de piaţa a bunului:" contractIpoteca.valoareBunDePiata |> map SetValoareBunDePiata ]
-
-            -- TODO: Continue here.
-            --
-            --
-            -- , li []
-            --     [ moneyField "Valoarea de inlocuire a bunului:"
-            --         contractIpoteca.valoareBunDeInlocuire
-            --         (\v -> callback { contractIpoteca | valoareBunDeInlocuire = v })
-            --     ]
-            -- , li []
-            --     [ moneyField "Valoarea de baza a creanţei garantate:"
-            --         contractIpoteca.valoareDeBazaCreanta
-            --         (\v -> callback { contractIpoteca | valoareDeBazaCreanta = v })
-            --     ]
-            -- , li [] [ largeTextField "Note:" contractIpoteca.note (\v -> callback { contractIpoteca | note = v }) ]
-            ]
+        , LargeTextField.view "Obiectul ipotecii:" contractIpoteca.obiect |> map SetObiect
+        , MoneyField.view "Valoarea de piaţa a bunului:" contractIpoteca.valoareBunDePiata |> map SetValoareBunDePiata
+        , MoneyField.view "Valoarea de inlocuire a bunului:" contractIpoteca.valoareBunDeInlocuire |> map SetValoareBunDeInlocuire
+        , MoneyField.view "Valoarea de baza a creanţei garantate:" contractIpoteca.valoareDeBazaCreanta |> map SetValoareDeBazaCreanta
+        , LargeTextField.view "Note:" contractIpoteca.note |> map SetNote
         ]
